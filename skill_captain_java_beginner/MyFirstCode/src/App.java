ff37ff8b764dd00c22c318308f920fd2bbb3a442
Prajjawal <prajjawaldy.zhcet@gmail.com>
2023-08-31 17:51:23 +0530

    completed day1 task

--- skill_captain_java_beginner/MyFirstCode/src/App.java
+++ skill_captain_java_beginner/MyFirstCode/src/App.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 public class App{
   public static void main(String[] args){
-    List myList = new ArrayList<>();
+    List<Integer> myList = new ArrayList<>();
     myList.add(1);
     myList.add(2);
     myList.add(3);
